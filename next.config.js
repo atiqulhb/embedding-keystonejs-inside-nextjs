6e4c0ef2f6e16cf19e379b359b9af414648da3dd
Atiqul Hoque Bhuiyan <atiqulbhuiyan@google.com>
2022-03-16 08:21:51 +0600

    first commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+const { withKeystone } = require("@keystone-6/core/next");
+
+module.exports = withKeystone({
+  reactStrictMode: true,
+});
+
